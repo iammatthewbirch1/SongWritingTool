74733bb128eaef7a1e2974f4ebfcf32db8adf621
iammatthewbirch1 <iammatthewbirch@gmail.com>
2024-01-16 23:32:20 +0000

    some progress on formatting the dictionary api

--- js/api/dictionary.js
+++ js/api/dictionary.js
@@ -15,11 +15,34 @@ export default class Dictionary {
             try {
                 const dictionaryResponse = await fetch(url, options);
                 const dictionaryResult = await dictionaryResponse.text();
-                this.savedDefinition = JSON.parse(dictionaryResult);
+                this.savedDefinition = this.parseResult(dictionaryResult);
                 resolve();
             } catch (error) {
                 console.error(error);
             }
         });
+    }
+
+    parseResult(result) {
+        result = JSON.parse(result);
+        let newResult = {};
+        newResult.wordTypes = [];
+        newResult.phonetic = result[0].phonetic;
+        newResult.word = result[0].word;
+
+        result.forEach(r => {
+            r.meanings.forEach(meaning => {
+                if(!newResult.wordTypes[meaning.partOfSpeech]) newResult.wordTypes[meaning.partOfSpeech] = [];
+                
+                newResult.wordTypes[meaning.partOfSpeech].push({
+                    'synonyms': meaning.synonyms,
+                    'antonyms': meaning.antonyms,
+                    'definitions': meaning.definitions.map(definition => { return {'definition': definition.definition, 'example': definition.example}})
+                });
+                 
+            });
+        });
+
+        return newResult;
     }
 } 
